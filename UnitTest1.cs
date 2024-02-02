4b81f5ef6919f7fb5d32ec2071f5b6be274545a4
Pierre-Eric <tavelpe@gmail.com>
2024-02-02 17:01:40 +0100

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TestJenkinsConsole
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
